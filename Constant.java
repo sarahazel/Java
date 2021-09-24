8f814e05c098c9c3a81569b32da6a661eeb56e09
sarahazel <81137827+sarahazel@users.noreply.github.com>
2021-09-24 07:56:41 +0700

    Update Constant.java

--- Constant.java
+++ Constant.java
@@ -3,12 +3,19 @@ public class Constants {
     /**
      * @param args the command line arguments
      */
-   public static final double CM_PER_INCH = 2.54;
+   
     public static void main(String[] args) {
         // TODO code application logic here
+        final double CM_PER_INCH = 2.54;
         double paperWidth = 8.5;
         double paperHeight = 11;
         System.out.println("Paper size in centimeters:"
          + paperWidth * CM_PER_INCH + "by" + paperHeight * CM_PER_INCH);
+        
+        
     }
+    
+}
+
+
     
